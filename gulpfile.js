9578747e10d3b3f2eb0f794d495904dca23d2f6d
Justen Robertson <justen@justen.us>
2017-05-04 09:45:09 -0700

    Sass build scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var pug = require("gulp-pug");
+var sass = require("gulp-sass");
 var webpack = require("webpack");
 var webpackConfig = require("./webpack.config");
 var del = require("del");
@@ -12,8 +13,18 @@ var mocha = require("gulp-mocha");
 var istanbul = require("gulp-babel-istanbul");
 */
 
+gulp.task("default", ["scripts", "styles", "markup"]);
+
 gulp.task("clean-scripts", function() {
 	return del(["target/scripts", "dist/scripts"]);
+});
+
+gulp.task("clean-styles", function() {
+	return del(["dist/styles"]);
+});
+
+gulp.task("clean-markup", function() {
+	return del(["dist/*.html"]);
 });
 
 gulp.task("scripts", ["clean-scripts"], function() {
@@ -22,10 +33,16 @@ gulp.task("scripts", ["clean-scripts"], function() {
 	.pipe(gulp.dest("target/scripts/"));
 });
 
-gulp.task("markup", function() {
+gulp.task("markup", ["clean-markup"], function() {
 	return gulp.src(["src/markup/*pug"])
 	.pipe(pug())
 	.pipe(gulp.dest("dist"))
+});
+
+gulp.task("styles", ["clean-styles"], function() {
+	return gulp.src(["src/styles/*scss"])
+	.pipe(sass().on("error", sass.logError))
+	.pipe(gulp.dest("dist/styles/"))
 });
 
 gulp.task("webpack", ["scripts"], function(callback) {
