0ce785fe3aa12389b014dbee64f38f149f1925a0
Justen Robertson <justen@justen.us>
2017-05-06 23:54:40 -0700

    migrated webpack config into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,52 +4,61 @@ var babel = require("gulp-babel");
 var pug = require("gulp-pug");
 var sass = require("gulp-sass");
 var webpack = require("webpack");
-var webpackConfig = require("./webpack.config");
 var del = require("del");
+var path = require("path");
 /*
 var babelRegister = require("babel-core/register");
 var exec = require("child_process").exec;
 var mocha = require("gulp-mocha");
 var istanbul = require("gulp-babel-istanbul");
 */
-gulp.task("clean-scripts", function() {
+
+const webpackConfig = {
+	entry:path.resolve(__dirname, "target/scripts/photonomix.js"),
+	devtool:"source-map",
+	output:{
+		filename:"photonomix.js",
+		path:path.resolve(__dirname, "dist/scripts")
+	},
+	plugins:[
+		new webpack.optimize.DedupePlugin(),
+		//new webpack.optimize.UglifyJsPlugin()
+  ]
+}
+
+gulp.task("clean:scripts", function() {
 	return del(["target/scripts/*", "dist/scripts/*"]);
 });
 
-gulp.task("clean-styles", function() {
+gulp.task("clean:styles", function() {
 	return del(["dist/styles/*"]);
 });
 
-gulp.task("clean-markup", function() {
+gulp.task("clean:markup", function() {
 	return del(["dist/*.html"]);
 });
 
-gulp.task("scripts", ["clean-scripts"], function() {
+gulp.task("scripts", ["clean:scripts"], function() {
 	return gulp.src(["src/scripts/*js"])
 	.pipe(babel())
 	.pipe(gulp.dest("target/scripts/"));
 });
 
-gulp.task("markup", ["clean-markup"], function() {
+gulp.task("markup", ["clean:markup"], function() {
 	return gulp.src(["src/markup/*pug"])
 	.pipe(pug())
 	.pipe(gulp.dest("dist"))
 });
 
-gulp.task("styles", ["clean-styles"], function() {
+gulp.task("styles", ["clean:styles"], function() {
 	return gulp.src(["src/styles/*scss"])
 	.pipe(sass().on("error", sass.logError))
 	.pipe(gulp.dest("dist/styles/"))
 });
 
+/* jshint unused:false */
 gulp.task("webpack", ["scripts"], function(callback) {
-	var myConfig = Object.create(webpackConfig);
-	myConfig.plugins = [
-		new webpack.optimize.DedupePlugin(),
-		//new webpack.optimize.UglifyJsPlugin()
-  ];
-	// run webpack
-	webpack(myConfig, function(err, stats) {
+	webpack(webpackConfig, function(err, stats) {
 		if(err) console.log(err);
 		/*
 		if (err) throw new gutil.PluginError('webpack', err);
