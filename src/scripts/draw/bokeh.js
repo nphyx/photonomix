37673a53fc92b296573469a483a418a2b012d423
Justen Robertson <justen@justen.us>
2017-11-15 15:05:51 -0800

    new background

--- src/scripts/draw/bokeh.js
+++ src/scripts/draw/bokeh.js
@@ -2,8 +2,8 @@
  * Derived from bokeh generator by Jack Rugile at [CodePen](http://codepen.io/jackrugile/pen/gaFub)
  */
 "use strict";
-let bgBuffer, bokehBuffer, bgCtx, bokehCtx, cw, ch, w, h, tau = Math.PI * 2,
-	parts = [], sizeBase, hue, opt, count, displayProps;
+import {shuffle} from "../photonomix.util.js";
+let bgBuffer, bokehBuffer, bgCtx, bokehCtx, tau = Math.PI * 2, parts = [], displayProps;
 
 function rand( min, max ) {
 	return Math.random() * (max - min) + min;
@@ -19,52 +19,12 @@ export function init(display) {
 	bgCtx = bgBuffer.context;
 	bokehBuffer = display.buffersByLabel.bokehFront; //buffer2;
 	bokehCtx = bokehBuffer.context;
-	updateProps();
-	displayProps.events.on("resize", updateProps);
-	// this can't get resized or we'd have to regenerate it, so set it up now
-	cw = bgBuffer.width = 1920;
-	ch = bgBuffer.height = 1920; 
-	w = bokehBuffer.width;
-	h = bokehBuffer.height;
 
-	sizeBase = cw + ch;
-	count = Math.floor(sizeBase * 0.1);
-	hue = rand(0, 360);
-	opt = {
-		radiusMin: sizeBase * 0.02,
-		radiusMax: sizeBase * 0.10,
-		blurMin: sizeBase * 0.01,
-		blurMax: sizeBase * 0.04,
-		hueMin: hue,
-		hueMax: hue + rand(25,75),
-		saturationMin: 10,
-		saturationMax: 70,
-		lightnessMin: 1,
-		lightnessMax: 5,
-		alphaMin: 0.1,
-		alphaMax: 0.4
-	}
-	bgCtx.fillStyle = "#000000";
-	bgCtx.fillRect(0, 0, cw, ch);
-	bgCtx.globalCompositeOperation = "lighter";
-	while(count--) {
-		let radius = rand(opt.radiusMin, opt.radiusMax),
-			blur = rand(opt.blurMin, opt.blurMax),
-			x = rand(0, cw),
-			y = rand(0, ch),
-			hue = rand(opt.hueMin, opt.hueMax),
-			saturation = rand(opt.saturationMin, opt.saturationMax),
-			lightness = rand(opt.lightnessMin, opt.lightnessMax),
-			alpha = rand(opt.alphaMin, opt.alphaMax);
+	let sizeBase = bgBuffer.width + bgBuffer.height;
+	let w = bokehBuffer.width;
+	let h = bokehBuffer.height;
 
-		bgCtx.shadowColor = hsla(hue, saturation, lightness, alpha);
-		bgCtx.shadowBlur = blur;
-		bgCtx.beginPath();
-		bgCtx.arc(x, y, radius, 0, tau);
-		bgCtx.fill();
-		bgCtx.closePath();
-	}
-	
+
 	parts.length = 0;
 	for( var i = 0; i < Math.floor( ( w + h ) * 0.01 ); i++ ) {
 		parts.push({
@@ -76,17 +36,77 @@ export function init(display) {
 			tick: rand( 0, 10000 )
 		});
 	}
+
+	generateBackground();
+	displayProps.events.on("resize", generateBackground);
+}
+
+export function generateBackground() {
+	let w = bokehBuffer.width;
+	let h = bokehBuffer.height;
+	let mind = Math.min(w, h);
+	let maxd = Math.max(w, h);
+	bgCtx.fillStyle = "black";
+	bgCtx.fillRect(0, 0, w, h);
+	let g = bgCtx.createLinearGradient(0, 0, w, h);
+	let colors = [
+		"rgba(255,64,64,1.0)",
+		"rgba(64,255,64,1.0)",
+		"rgba(64,64,255,1.0)"
+	];
+	shuffle(colors);
+	g.addColorStop(0.0, colors[0]);
+	g.addColorStop(0.5, colors[1]);
+	g.addColorStop(1.0, colors[2]);
+	bgCtx.fillStyle = g;
+	bgCtx.fillRect(0, 0, w, h);
+	g = bgCtx.createLinearGradient(0, h, w, 0);
+	colors = [
+		"rgba(255,64,64,0.8)",
+		"rgba(64,255,64,0.8)",
+		"rgba(64,64,255,0.8)"
+	];
+	shuffle(colors);
+	g.addColorStop(0.0, colors[0]);
+	g.addColorStop(0.5, colors[1]);
+	g.addColorStop(1.0, colors[2]);
+	bgCtx.fillStyle = g;
+	bgCtx.fillRect(0, 0, w, h);
+
+	g = bgCtx.createRadialGradient(w/2, h/2, maxd/2, w/2, h/2, 0);
+	let rad = ((maxd - mind) / maxd);
+	console.log(rad);
+	g.addColorStop(1, "rgba(64,64,64,0.6)");
+	g.addColorStop(rad + 0.06, "rgba(0,0,0,0.8)");
+	g.addColorStop(rad + 0.05, "rgba(128,128,128,1.0)");
+	g.addColorStop(rad + 0.04, "rgba(255,255,255,0.6)");
+	g.addColorStop(rad + 0.03, "rgba(255,255,255,0.8)");
+	g.addColorStop(rad + 0.025, "rgba(255,255,255,0.8)");
+	g.addColorStop(rad + 0.005, "rgba(255,255,255,0.5)");
+	/*
+	g.addColorStop(rad + 0.07, "rgba(0,0,0,0.8)");
+	g.addColorStop(rad + 0.085, "rgba(128,128,128,1.0)");
+	g.addColorStop(rad + 0.10, "rgba(255,255,255,0.6)");
+	g.addColorStop(rad + 0.11, "rgba(255,255,255,0.8)");
+	g.addColorStop(rad + 0.13, "rgba(255,255,255,0.8)");
+	g.addColorStop(rad + 0.16, "rgba(255,255,255,0.5)");
+	*/
+	g.addColorStop(0, "rgba(255,255,255,0.01)");
+	bgCtx.fillStyle = g;
+	bgCtx.globalCompositeOperation = "luminosity";
+	bgCtx.fillRect(0, 0, w, h);
+	bgCtx.globalCompositeOperation = "source-in";
 }
 
 export function draw() {
 	var i = parts.length;
+	let w = bokehBuffer.width;
+	let h = bokehBuffer.height;
 	bokehCtx.fillStyle = "rgba(0,0,0,0)";
 	bokehCtx.globalCompositeOperation = "source-over";
-	bokehCtx.clearRect(0,0,cw,ch);
+	bokehCtx.clearRect(0, 0, w, h);
 	bokehCtx.shadowBlur = 15;
 	bokehCtx.shadowColor = "#fff";
-	w = bokehBuffer.width;
-	h = bokehBuffer.height;
 	while(i--) {
 		var part = parts[i];
 		
@@ -99,16 +119,11 @@ export function draw() {
 		bokehCtx.fillStyle = hsla(0, 0, 100, 0.03 + Math.cos( part.tick * 0.02 ) * 0.01);
 		bokehCtx.fill();
 		
-		if(part.x - part.radius > cw) part.x = -part.radius;
+		if(part.x - part.radius > w) part.x = -part.radius;
 		if(part.x + part.radius < 0) part.x = w + part.radius;
-		if(part.y - part.radius > ch) part.y = -part.radius;
+		if(part.y - part.radius > h) part.y = -part.radius;
 		if(part.y + part.radius < 0) part.y = h + part.radius;
 		
 		part.tick++;
 	}
-}
-
-function updateProps() {
-	bokehBuffer.width = displayProps.width;
-	bokehBuffer.height = displayProps.height;
 }
