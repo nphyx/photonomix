3e027c9c8861769ecc3c854777f424d07a3e95fd
Justen Robertson <justen@justen.us>
2017-11-16 14:24:09 -0800

    paths relative to index for compatibility

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -2,7 +2,7 @@
 "use strict";
 if("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-		navigator.serviceWorker.register("/sw.js")
+		navigator.serviceWorker.register("./sw.js")
 		.then(
 			(registration) => console.log("worker registered", registration.scope),
 			(err) => console.log("worker registration failed", err)
